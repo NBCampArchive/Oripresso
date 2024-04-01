551aaa9fcb75ad9b91d29af6e0d65cacccd7b573
devpark435 <devpark435@gmail.com>
2024-04-01 15:08:00 +0900

    Setting #1 - Create Project

---
+++ Oripresso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oripresso
+//
+//  Created by 박현렬 on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
