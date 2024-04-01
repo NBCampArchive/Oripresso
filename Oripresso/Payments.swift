cc57fa5bf0118a0392e65d8cd212699eab744704
DDattj <20191886@sungshin.ac.kr>
2024-04-01 20:16:00 +0900

    이미지 싱글스케일로 바꾸기

--- Oripresso/Payments.swift
+++ Oripresso/Payments.swift
@@ -6,7 +6,21 @@
 //
 
 import Foundation
+import UIKit
 
-class Payments {
+class Payments: ViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myImage = UIImage(named: "영수증")
+        myImage?.layer.shadowColor = UIColor.black.cgColor
+        myImage?.layer.shadowOpacity = 1
+        myImage?.layer.shadowOffset = CGSize.zero
+        myImage?.layer.shadowRadius = 10
+        myImage?.layer.shadowPath = UIBezierPath(rect: myImage?.bounds ?? CGRect.zero).cgPath
+        myImage?.layer.shouldRasterize = false
+        myImage?.layer.cornerRadius = 10
+        myImage?.clipsToBounds = true
+    }
 }
