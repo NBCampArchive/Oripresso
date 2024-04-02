3745468bcf2b9c6dc4466a1a7b2b47a03260ef40
DDattj <20191886@sungshin.ac.kr>
2024-04-02 18:26:18 +0900

    영수증 내려오는 애니메이션 추가 -> 주석 달기

--- Oripresso/Payments.swift
+++ Oripresso/Payments.swift
@@ -20,29 +20,29 @@ class Payments: ViewController {
         animatebutton()
     }
     
+    //중간쯤부터 보이기 시작하면서 내려오는 애니메이션 의도
     func animateReceipt() {
-        // 초기 상태 설정: receipt 이미지뷰를 아래로 숨김
+        // 초기 상태 설정: receipt 이미지뷰의 위치를 이동시킴. 원래 높이에서 1/10 만큼 위로 이동 , opacity는 0.
         receipt.transform = CGAffineTransform(translationX: 0, y: -self.receipt.frame.height/10)
-        receipt.alpha = 0 // 초기 opacity를 0으로 설정
+        receipt.alpha = 0
         
         // 애니메이션 적용
-        UIView.animate(withDuration: 1.6, delay: 0.0, options: [.curveEaseOut], animations: {
-            // 애니메이션 처리: receipt 이미지뷰를 원래 위치로 이동하면서 opacity를 1로 변경
+        // 애니메이션 처리: receipt 이미지뷰를 원래 위치로 이동하면서 opacity를 1로 변경. 속도는 1.6, 딜레이 0.7, ease-out 애니메이션(점점 느려지기) 사용
+        // 애니메이션 끝나면 이후 동작 없음
+        UIView.animate(withDuration: 1.6, delay: 0.7, options: [.curveEaseOut], animations: {
             self.receipt.transform = .identity
-            self.receipt.alpha = 1 // 최종 opacity
+            self.receipt.alpha = 1
         }, completion: nil)
     }
     
+    //메인화면 이동 버튼도 동일하게 적용
     func animatebutton() {
-        // 초기 상태 설정: moveToMain 버튼을 아래로 숨김
-        moveToMain.transform = CGAffineTransform(translationX: 0, y: -self.receipt.frame.height/10) // receipt 이미지뷰와 동일한 위치로 설정
-        moveToMain.alpha = 0 // 초기 opacity를 0으로 설정
+        moveToMain.transform = CGAffineTransform(translationX: 0, y: -self.receipt.frame.height/10)
+        moveToMain.alpha = 0
         
-        // 애니메이션 적용
-        UIView.animate(withDuration: 1.6, delay: 0.0, options: [.curveEaseOut], animations: {
-            // 애니메이션 처리: moveToMain 버튼을 원래 위치로 이동하면서 opacity를 1로 변경
+        UIView.animate(withDuration: 1.6, delay: 0.7, options: [.curveEaseOut], animations: {
             self.moveToMain.transform = .identity
-            self.moveToMain.alpha = 1 // 최종 opacity
+            self.moveToMain.alpha = 1
         }, completion: nil)
     }
 }
