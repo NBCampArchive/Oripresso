53d467b40c519fce7423538b8fbc87ca5781846e
DDattj <20191886@sungshin.ac.kr>
2024-04-02 17:56:20 +0900

    영수증 내려오는 애니메이션 추가

--- Oripresso/Payments.swift
+++ Oripresso/Payments.swift
@@ -16,7 +16,33 @@ class Payments: ViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        animateReceipt()
+        animatebutton()
     }
     
+    func animateReceipt() {
+        // 초기 상태 설정: receipt 이미지뷰를 아래로 숨김
+        receipt.transform = CGAffineTransform(translationX: 0, y: -self.receipt.frame.height/10)
+        receipt.alpha = 0 // 초기 opacity를 0으로 설정
+        
+        // 애니메이션 적용
+        UIView.animate(withDuration: 1.6, delay: 0.0, options: [.curveEaseOut], animations: {
+            // 애니메이션 처리: receipt 이미지뷰를 원래 위치로 이동하면서 opacity를 1로 변경
+            self.receipt.transform = .identity
+            self.receipt.alpha = 1 // 최종 opacity
+        }, completion: nil)
+    }
+    
+    func animatebutton() {
+        // 초기 상태 설정: moveToMain 버튼을 아래로 숨김
+        moveToMain.transform = CGAffineTransform(translationX: 0, y: -self.receipt.frame.height/10) // receipt 이미지뷰와 동일한 위치로 설정
+        moveToMain.alpha = 0 // 초기 opacity를 0으로 설정
+        
+        // 애니메이션 적용
+        UIView.animate(withDuration: 1.6, delay: 0.0, options: [.curveEaseOut], animations: {
+            // 애니메이션 처리: moveToMain 버튼을 원래 위치로 이동하면서 opacity를 1로 변경
+            self.moveToMain.transform = .identity
+            self.moveToMain.alpha = 1 // 최종 opacity
+        }, completion: nil)
+    }
 }
