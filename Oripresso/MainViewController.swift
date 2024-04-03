35decf74f9e829f080a45abbc0292f6c632c8e19
devpark435 <devpark435@gmail.com>
2024-04-03 10:46:33 +0900

    design #11 - Delete ConflictCode

--- Oripresso/MainViewController.swift
+++ Oripresso/MainViewController.swift
@@ -18,9 +18,6 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        uiTableView.dataSource = self
-        uiTableView.delegate = self
     }
 }
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
