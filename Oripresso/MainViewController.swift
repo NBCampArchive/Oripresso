0cf07fd0b9f6a5098d2f0300bbdab566828117ee
신승현 <sinseunghyeon313@gmail.com>
2024-04-03 17:44:15 +0900

    [Feat] Cell 클릭시 데이터값 배열에 저장 구현 #5
    selectedMenu 배열에 저장하고 삭제하는 기능 구현

--- Oripresso/MainViewController.swift
+++ Oripresso/MainViewController.swift
@@ -17,16 +17,33 @@ class MainViewController: UIViewController {
     @IBOutlet weak var segment: UISegmentedControl!
     @IBOutlet weak var floatingButton: UIButton!
     
-    let data: [MenuData] = [MenuData(name: "Americano", type: .coffee),
-                            MenuData(name: "Juice", type: .nonCoffee),
-                            MenuData(name: "Cake", type: .cake),
-                            MenuData(name: "Bread", type: .bread)]
+    let data: [MenuData] = [MenuData(name: "아메리카노", type: .coffee),
+                            MenuData(name: "초코블렌디드", type: .nonCoffee),
+                            MenuData(name: "당근케이크", type: .cake),
+                            MenuData(name: "소금빵", type: .bread),
+                            MenuData(name: "에스프레소", type: .coffee),
+                            MenuData(name: "레몬에이드", type: .nonCoffee),
+                            MenuData(name: "레드벨벳케이크", type: .cake),
+                            MenuData(name: "식빵", type: .bread),
+                            MenuData(name: "아이스아메리카노", type: .coffee),
+                            MenuData(name: "민트초코블렌디드", type: .nonCoffee),
+                            MenuData(name: "티라미수", type: .cake),
+                            MenuData(name: "크루와상", type: .bread),
+                            MenuData(name: "카페라떼", type: .coffee),
+                            MenuData(name: "자몽에이드", type: .nonCoffee),
+                            MenuData(name: "초코케이크", type: .cake),
+                            MenuData(name: "빵", type: .bread)]
+    var coffee: [MenuData] = []
+    var nonCoffee: [MenuData] = []
+    var cake: [MenuData] = []
+    var bread: [MenuData] = []
     var selectedMenu: [MenuData] = []
     
     func tableViewDelegate() {
         uiTableView.dataSource = self
         uiTableView.delegate = self
         uiTableView.reloadData()
+        uiTableView.allowsMultipleSelection = true
     }
     
     @IBAction func segmentedControlSelected(_ sender: Any) {
@@ -45,8 +62,24 @@ class MainViewController: UIViewController {
         
         if let pushVC = storyboard.instantiateViewController(withIdentifier: "OrderList") as? OrderListViewController {
             self.navigationController?.pushViewController(pushVC, animated: true)
+            print(selectedMenu)
         } else {
             print(selectedMenu)
+        }
+    }
+    
+    func divideType(datas: [MenuData]) {
+        for data in datas {
+            switch data.type {
+            case .coffee:
+                coffee.append(data)
+            case .nonCoffee:
+                nonCoffee.append(data)
+            case .cake:
+                cake.append(data)
+            case .bread:
+                bread.append(data)
+            }
         }
     }
 }
@@ -88,7 +121,51 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewTableCell", for: indexPath) as! MainViewTableCell
         // setCell 메서드를 호출하여 셀의 데이터를 설정합니다.
         cell.setCell(image: "americano", title: "title", description: "description", price: "4500")
-        
-        return cell
+        divideType(datas: data)
+        switch segment.selectedSegmentIndex {
+        case 0:
+            cell.setCell(image: "americano", title: coffee[indexPath.row].name, description: "desciption", price: "4500")
+            return cell
+        case 1:
+            cell.setCell(image: "americano", title: nonCoffee[indexPath.row].name, description: "desciption", price: "4500")
+            return cell
+        case 2:
+            cell.setCell(image: "americano", title: cake[indexPath.row].name, description: "desciption", price: "4500")
+            return cell
+        case 3:
+            cell.setCell(image: "americano", title: bread[indexPath.row].name, description: "desciption", price: "4500")
+            return cell
+        default:
+            return cell
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch segment.selectedSegmentIndex {
+        case 0:
+            selectedMenu.append(coffee[indexPath.row])
+        case 1:
+            selectedMenu.append(nonCoffee[indexPath.row])
+        case 2:
+            selectedMenu.append(cake[indexPath.row])
+        case 3:
+            selectedMenu.append(bread[indexPath.row])
+        default:
+            break
+        }
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        switch segment.selectedSegmentIndex {
+        case 0:
+            self.selectedMenu = selectedMenu.filter { $0.name != coffee[indexPath.row].name}
+        case 1:
+            self.selectedMenu = selectedMenu.filter { $0.name != nonCoffee[indexPath.row].name}
+        case 2:
+            self.selectedMenu = selectedMenu.filter { $0.name != cake[indexPath.row].name}
+        case 3:
+            self.selectedMenu = selectedMenu.filter { $0.name != bread[indexPath.row].name}
+        default:
+            break
+        }
     }
 }
