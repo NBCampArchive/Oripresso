783a5682ffaf8a9043afedb8ad0f6cdb917223b9
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-03 21:04:19 +0900

    #4 - 주문하기/취소하기 버튼 연결

--- Oripresso/OrderListViewController.swift
+++ Oripresso/OrderListViewController.swift
@@ -23,6 +23,30 @@ class OrderListViewController: UIViewController {
     
     @IBOutlet weak var orderListTableView: UITableView!
     
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "주문을 취소하시겠습니까?", message: "담긴 주문은 전체 삭제 되고 메인화면으로 돌아갑니다", preferredStyle: .alert)
+        let orderCancelAction = UIAlertAction(title: "아니요", style: .destructive) { action in
+        }
+        let orderDidTapButton = UIAlertAction(title: "예", style: .default) { action in
+        }
+        alert.addAction(orderCancelAction)
+        alert.addAction(orderDidTapButton)
+        
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func tapOrderButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "주문하시겠습니까?", message: nil, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "아니요", style: .destructive) { action in
+        }
+        let didTapButton = UIAlertAction(title: "예", style: .default) { action in
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(didTapButton)
+        
+        self.present(alert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         orderListTableView.dataSource = self
