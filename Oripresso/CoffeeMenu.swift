495714bb5126c5469d8a6932edbaa6e0e21b9fad
devpark435 <devpark435@gmail.com>
2024-04-04 10:58:38 +0900

    feat #16 - Add CoffeeJson Struct File

--- Oripresso/CoffeeMenu.swift
+++ Oripresso/CoffeeMenu.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+struct CafeMenu: Codable {
+    let coffee: Category
+    let nonCoffee: Category
+    let desert: Category
+    let bread: Category
+    
+    enum CodingKeys: String, CodingKey {
+        case coffee = "Coffee"
+        case nonCoffee = "Non-Coffee"
+        case desert = "Desert"
+        case bread = "Bread"
+    }
+}
+
+struct Category: Codable {
+    let totalPages: Int
+    let menus: [Menu]
+}
+
+struct Menu: Codable {
+    let name: String
+    let price: Int
+    let description: String
+}
